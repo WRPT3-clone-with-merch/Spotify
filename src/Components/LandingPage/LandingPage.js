7f6ca9dbdec159e141d9ed8aebaaf38adfb0a599
Logan Banning <loganboganb@gmail.com>
2021-12-16 21:33:33 -0700

    setting up landing page

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -3,12 +3,13 @@ import SideBar from '../SideBar/SideBar';
 import TopNav from '../TopNav/TopNav';
 import axios from 'axios';
 import './LandingPage.css';
+import Logo from '../../images/Spotify_Logo_RGB_White.png';
 
 const LandingPageComponent = (props) => {
 
   return (
-    <div>
-
+    <div className='parent-div'>
+      <img src={Logo} alt='Logo' className='logo-landing-page' />
     </div>
   )
 }
