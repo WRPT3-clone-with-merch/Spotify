6d16b1fa0a25084ac004c3ca32f604d3e92017a3
Jesse Kirkeeng <lmjtjesse@gmail.com>
2021-12-16 20:12:57 -0700

    massive, express and server setup

---
+++ server/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require("massive");
+const session = require("express-session");
+
+const { SESSION_SECRET, SERVER_PORT, CONNECTION_STRING } = process.env;
+
+const app = express();
+
+massive({
+	connectionString: CONNECTION_STRING,
+	ssl: { rejectUnauthorized: false },
+}).then((db) => {
+	app.set("db", db);
+	console.log("db connected");
+}).catch(err => console.log(err));
+
+app.use(
+	session({
+		resave: true,
+		saveUninitialized: false,
+		secret: SESSION_SECRET,
+		cookie: { maxAge:  60000 * 60 * 24 * 90}
+}));
+app.use(express.json());
+
+// app.get('/api/')
+
+app.listen(SERVER_PORT, () => console.log(`${SERVER_PORT}`))
