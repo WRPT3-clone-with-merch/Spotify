00de557556e6ad63eb0c3282a6e24c794b1d5399
Kevin-Hutchings <ktmhutchings@gmail.com>
2021-12-21 22:23:51 -0700

    tested using access token and pulling data from spotify api

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import axios from 'axios';
-import './LandingPage.css';
-import Logo from '../../images/Spotify_Logo_RGB_White.png';
-import { Link } from 'react-router-dom';
+import React from "react";
+import axios from "axios";
+import "./LandingPage.css";
+import Logo from "../../images/Spotify_Logo_RGB_White.png";
+import { Link } from "react-router-dom";
 
 const LandingPageComponent = (props) => {
   const {
@@ -12,17 +12,19 @@ const LandingPageComponent = (props) => {
   } = process.env;
 
   const handleLogin = () => {
-    window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${REACT_APP_REDIRECT_URL}&show_dialog=true`;
+    window.location = `${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${REACT_APP_REDIRECT_URL}&scope=streaming%20playlist-read-private&show_dialog=true`;
   };
 
   return (
-    <div className='parent-div'>
-      <img src={Logo} alt='Logo' className='logo-landing-page' />
-      <Link to='/homepage'>
-      <button className='landing-btn' onClick={handleLogin}>Connect to Spotify</button>
+    <div className="parent-div">
+      <img src={Logo} alt="Logo" className="logo-landing-page" />
+      <Link to="/homepage">
+        <button className="landing-btn" onClick={handleLogin}>
+          Connect to Spotify
+        </button>
       </Link>
     </div>
-  )
+  );
 };
 
 export default LandingPageComponent;
--- src/Components/Playlists/Playlists.js
+++ src/Components/Playlists/Playlists.js
@@ -1,17 +1,40 @@
-import React, { useState } from 'react';
-import SideBar from '../SideBar/SideBar';
-import TopNavLibrary from '../TopNav/TopNavLibrary';
-import axios from 'axios';
-import './Playlists.css';
+import React, { useState, useEffect } from "react";
+import SideBar from "../SideBar/SideBar";
+import TopNavLibrary from "../TopNav/TopNavLibrary";
+import axios from "axios";
+import "./Playlists.css";
 
-const PlaylistsComponent = (props) => {
+const Playlists = (props) => {
+  const [list, setList] = useState({})
+
+  useEffect(() => {
+    async function getToken() {
+      const response = await axios.get("/auth/token");
+      try {
+        axios.get("https://api.spotify.com/v1/me/playlists", {
+          params: { limit: 1, offset: 0 },
+          headers: {
+            Accept: "application/json",
+            Authorization: "Bearer " + response.data.access_token,
+            "Content-Type": "application/json",
+          },
+        }).then(({data}) => setList(data))
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getToken();
+  }, []);
+
+  console.log(list)
 
   return (
     <div>
       <SideBar />
       <TopNavLibrary />
     </div>
-  )
-}
+  );
+};
 
-export default PlaylistsComponent;
+export default Playlists;
