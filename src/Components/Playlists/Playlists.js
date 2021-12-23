ce34e8e6e2bf8d98c04b5d991f253a7adbbee5e3
Kevin-Hutchings <ktmhutchings@gmail.com>
2021-12-22 22:23:52 -0700

    moved auth function to utils, started work on playlist previews

--- src/Components/Playlists/Playlists.js
+++ src/Components/Playlists/Playlists.js
@@ -2,37 +2,47 @@ import React, { useState, useEffect } from "react";
 import SideBar from "../SideBar/SideBar";
 import TopNavLibrary from "../TopNav/TopNavLibrary";
 import axios from "axios";
+import { useToken } from "../../utils";
 import "./Playlists.css";
 
 const Playlists = (props) => {
-  const [list, setList] = useState({})
+  const [list, setList] = useState([]);
+  const token = useToken();
 
   useEffect(() => {
-    async function getToken() {
-      const response = await axios.get("/auth/token");
-      try {
-        axios.get("https://api.spotify.com/v1/me/playlists", {
-          params: { limit: 1, offset: 0 },
+    try {
+      axios
+        .get("https://api.spotify.com/v1/me/playlists", {
+          params: { limit: 10, offset: 0 },
           headers: {
-            Accept: "application/json",
-            Authorization: "Bearer " + response.data.access_token,
-            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
           },
-        }).then(({data}) => setList(data))
-      } catch (err) {
-        console.log(err);
-      }
+        })
+        .then(({ data }) => setList(data.items));
+    } catch (err) {
+      console.log(err);
     }
+  }, [token]);
 
-    getToken();
-  }, []);
+  console.log(list);
 
-  console.log(list)
+  const playlistMap = list.map((list) => {
+    return (
+      <div className="playlist-preview" key={list.id}>
+        <img className="playlist-image" src={list.images[0].url} />
+        <h3>{list.name}</h3>
+        <p className="playlist-description">{list.description}</p>
+      </div>
+    );
+  });
 
   return (
-    <div>
-      <SideBar />
-      <TopNavLibrary />
+    <div className="playlists">
+      <div className="playlist-container">
+        {playlistMap}
+        <SideBar />
+        <TopNavLibrary />
+      </div>
     </div>
   );
 };
