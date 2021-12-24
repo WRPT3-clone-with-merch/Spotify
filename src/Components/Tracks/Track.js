01ea45418bdbc2934ea5fca6efa9718a1fb23b58
Jesse Kirkeeng <lmjtjesse@gmail.com>
2021-12-23 19:41:21 -0700

    update track

--- src/Components/Tracks/Track.js
+++ src/Components/Tracks/Track.js
@@ -1,10 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useToken } from "../../utils";
 
 const Track = (props) => {
-	const [track, setTrack] = useState('')
+	const [track, setTrack] = useState([])
+	const token = useToken();
 
-	const getTrack = axios.get(`https://api.spotify.com/v1/tracks/{id}`)
-		.then
+	useEffect(() => {
+		axios.get(`https://api.spotify.com/v1/tracks/{id}`)
+}, [token]);
 
 	return (
 		<div>
