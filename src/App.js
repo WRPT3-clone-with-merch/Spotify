8a890f42e362502f62f236771d0aeedcb8cc1203
Logan Banning <loganboganb@gmail.com>
2021-12-23 18:44:25 -0700

    working on fixing app js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import routes from "./routes";
 import "./App.css";
-import WebPlayback from "./Components/WebPlayback/WebPlayback"
+import WebPlayback from "./Components/WebPlayback/WebPlayback";
+import axios from 'axios';
 
 const code = new URLSearchParams(window.location.search).get("code");
 
-function App() {
+const App = (props) => {
+
+  const { token, setToken } = useState(' ');
+
+  const getToken = async () => {
+    const response = await axios.get('/auth/token', {access_token})
+    setToken(response.access_token);
+  };
+
   return (
     <div className="App">
       {routes}
+      {!token && }
+      {token && 
     <WebPlayback />
+      }
     </div>
   );
 }
