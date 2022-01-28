0f45efa756c6d9f8f483ff6f85f27d5ae06ad2ef
Kevin-Hutchings <ktmhutchings@gmail.com>
2022-01-27 22:31:32 -0700

    Recommendations works with search results. removed a few console.logs. replaced url in axios requests with variable from utils. deleted musicplayer component

--- src/Components/Tracks/Tracks.js
+++ src/Components/Tracks/Tracks.js
@@ -1,47 +1,46 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useToken } from "../../utils";
+import { useToken, SpotifyURL } from "../../utils";
 import SideBar from "../SideBar/SideBar";
 import TopNavLibrary from "../TopNav/TopNavLibrary";
 import { Link } from "react-router-dom";
 
 const Tracks = (props) => {
-	const [tracks, setTracks] = useState([]);
-	const token = useToken();
+  const [tracks, setTracks] = useState([]);
+  const token = useToken();
 
-	useEffect(() => {
-		try {
-		axios.get(`https://api.spotify.com/v1/tracks`, {
-			headers: {Authorization: `Bearer ${token}`}
-		})
-		.then(res => setTracks(res))
-	}
-		catch (err) {
+  useEffect(() => {
+    try {
+      axios
+        .get(`${SpotifyURL}/tracks`, {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => setTracks(res));
+    } catch (err) {
       console.log(err);
-    };
-}, [token]);
+    }
+  }, [token]);
 
-let myTracks = tracks.map(song => {
-	return (
-		<Link to={`/track/${song.id}`} >
+  let myTracks = tracks.map((song) => {
+    return (
+      <Link to={`/track/${song.id}`}>
         <section key={song.id}>
-          <img src={song.track.album.images[2].url}/>
+          <img src={song.track.album.images[2].url} />
           <h2>{song.track.name}</h2>
         </section>
-    </Link>
-	);
-});
-console.log(myTracks)
+      </Link>
+    );
+  });
 
-return (
-	<div >
-		<div >
-			{myTracks}
-			<SideBar />
-			<TopNavLibrary />
-		</div>
-	</div>
-);
+  return (
+    <div>
+      <div>
+        {myTracks}
+        <SideBar />
+        <TopNavLibrary />
+      </div>
+    </div>
+  );
 };
 
 export default Tracks;
